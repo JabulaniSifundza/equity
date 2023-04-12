36082e1ebcbec06587f74e48b736f3c24d02c590
Jabulani Sifundza <jjsifundza@gmail.com>
2023-04-12 17:45:10 +0200

    Hosting on vercel

--- equity/settings.py
+++ equity/settings.py
@@ -54,11 +54,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'equity.urls'
+FRONTEND_DIR = BASE_DIR / "researchApp"
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [FRONTEND_DIR / "dist"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    FRONTEND_DIR / "dist",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
